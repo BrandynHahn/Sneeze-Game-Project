9dc5c0d6c19511cfff06db110d079f7ebdd5d8f7
JReevesHU <98060331+JReevesHU@users.noreply.github.com>
2022-02-14 15:31:45 -0500

    Working on adult movement script

--- SneezeGameFile/Assets/Scripts/AdultCharacter.cs
+++ SneezeGameFile/Assets/Scripts/AdultCharacter.cs
@@ -4,10 +4,23 @@ using UnityEngine;
 
 public class AdultCharacter : MonoBehaviour
 {
+    //Calling the RidgidBody
+    private Rigidbody2D myRigidbody;
+
+    //Speed for Adult
+    public float speed = 1f;
+
+    public bool isWalking;
+
+    public float walkTime;
+
+    public float waitTime; 
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        myRigidbody = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
